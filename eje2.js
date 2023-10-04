651a0b7a0dbbd527b153e4d9a09abbb4c8584728
brayanAcosta <brayanacosta364@gmail.com>
2023-10-04 09:18:29 -0500

    ejeDosTema3

---
+++ eje2.js
@@ -0,0 +1,26 @@
+//Dado un monto calcular el descuento considerando que por encima de 
+//100 el descuento es el 10% y por debajo de 100 el descuento es el 2%.
+
+//declaracion de variables
+var monto, descuento1=0.1, descuento2=0.02, montoConDescuento, descuento, opcion
+
+do {
+    opcion=parseInt(prompt("elija la opcion del menu: 1.hacer proceso 2.salir"))
+    switch (opcion) {
+        case 1:
+            monto=parseInt(prompt("ingrese el monto"));
+            switch (true) {
+            case (monto>=100):
+            montoConDescuento=monto-(monto*descuento1)
+            descuento=monto*descuento1
+            alert("se otorga un descuento del 10% equivalente a " +descuento+ " , el monto con descuento es de "+ montoConDescuento);
+        break;
+        default:
+            montoConDescuento=monto-(monto*descuento2)
+            descuento=monto*descuento2
+            alert("se otorga un descuento del 2% equivalente a " +descuento+ " , el monto con descuento es de "+ montoConDescuento);
+        break;
+        }
+    break;
+    }
+} while (opcion !=2);
